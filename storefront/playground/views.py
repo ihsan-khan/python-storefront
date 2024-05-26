6193bf22b76aa6a720df68a23bc20eab0209e3aa
unknown <ehsankhan0577@gmail.com>
2024-05-26 10:00:30 +0500

    using templates

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('hello world')
+    # return HttpResponse('hello world')
+    return render(request,'index.html',{'name':'ihsaan'})
