30faa2db6af9ba8b326eaf36f9c06840ae2ab9c5
unknown <ehsankhan0577@gmail.com>
2024-05-26 12:42:54 +0500

    models with ManyToMany relationship

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    # product_set
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -11,6 +15,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
