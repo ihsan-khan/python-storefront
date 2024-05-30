070170349d4607166257f5c56088035815dce01b
unknown <ehsankhan0577@gmail.com>
2024-05-30 11:59:07 +0500

    create and run migrations

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -3,16 +3,17 @@ from django.db import models
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
-class Promotion(models.Model):
+class  Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
     # product_set
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_length=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'store',
+    'likes',
+    'tags',
 ]
 
 MIDDLEWARE = [
