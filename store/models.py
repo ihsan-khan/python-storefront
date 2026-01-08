8c5a866175fba252e5e4e08e1304b41f580c506f
ihsan-khan <ehsankhan0577@gmail.com>
2026-01-08 11:27:31 +0500

    customer model were added

--- store/models.py
+++ store/models.py
@@ -11,3 +11,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+# customer model
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
