de5a95101b939ad874ceb6ea0074e6f4f99b457f
ihsan-khan <ehsankhan0577@gmail.com>
2026-01-08 11:18:29 +0500

    product model were created

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# product model
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
