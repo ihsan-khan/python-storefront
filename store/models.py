aca1d3fd6174942e9ab4f5b124751a4c8fd6a34e
ihsan-khan <ehsankhan0577@gmail.com>
2026-01-12 09:41:45 +0500

    order item were added

--- store/models.py
+++ store/models.py
@@ -62,3 +62,13 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.street}, {self.city}, {self.country}"
+    
+# order item model
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.title} @ {self.unit_price}"
