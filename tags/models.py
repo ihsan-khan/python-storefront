f2165f4ce8765c5122e15409cf664fdcfa514b6a
ihsan-khan <ehsankhan0577@gmail.com>
2026-01-13 12:40:01 +0500

    generic relation were added to tag app

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+# tag model
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+# tag-item relationship model
+class TagItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f"{self.tag.title} - {self.content_type} ({self.object_id})"
